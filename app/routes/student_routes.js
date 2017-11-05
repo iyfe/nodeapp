34e90ab22f8022730afb8cdc2b4f61b164aab923
Ifunanya Ukachukwu <icukachuk@gmail.com>
2017-11-05 17:12:19 -0500

    update app

--- app/routes/student_routes.js
+++ app/routes/student_routes.js
@@ -74,27 +74,44 @@ app.get('/student/all', function(req, res, next){
 
 
 // Edit student Page 
-app.get('/student/edit', function(req, res, next){
-  let id = req.body.id;
-  res.render('editstudent');
+app.get('/student/edit/:id', function(req, res, next){
+  const id= { '_id': new ObjectID(req.params.id) };
+   db.collection('students').findOne( id,(err,item) =>{
+    if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        if(!item){
+          res.render('searchstudent', {
+        error: 'User does not exist'
+      });
+        }else {
+          res.render('editstudent',{student:item});
+        }
+      } 
+  });
+  
 });
 
-app.put('/student/edit', (req, res,next) => {
-  const id= { '_id': new ObjectID(req.body.id) };
-  const student = {
-     first_name :  req.body.first_name,
+app.post('/student/edit', (req, res,next) => {
+
+  var myquery = { _id:new ObjectID(req.body.id)};
+  var newvalues = 
+    {
+      first_name :  req.body.first_name,
      last_name :  req.body.last_name,
      email :  req.body.email,
      phone : req.body.phone,
      image : req.body.image,
      next_of_kin :req.body.next_of_kin,
      nok_phone :req.body.nok_phone
-  };
-    db.collection('students').update(id, student, (err, result) => {
+    };
+  db.collection('students').update(myquery, {$set:newvalues}, function(err,result) {
       if (err) {
           res.send({'error':'An error has occurred'});
       } else {
-          res.render('liststudent',{student:result})
+        if(result.result.nModified === 1){
+          res.render('liststudent',{student:newvalues});
+        }
       } 
     });
 
@@ -104,7 +121,6 @@ app.post('/student/delete/:id', (req, res) => {
     const id = req.params.id;
     const details = { '_id': new ObjectID(id) };
     db.collection('students').deleteOne(details, (err, item) => {
- // db.collection('students').deleteOne(myquery, function(err, obj) {
       if (err) {
         res.send({'error':'An error has occurred'});
       } else {
